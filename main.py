478ef26677c65342fde297786e720ebcd6887557
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-09-29 17:01:01 +0300

    Clean names, add type hints

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Callable, Any
 
 
 class DataRow(NamedTuple):
@@ -12,15 +12,17 @@ class DataRow(NamedTuple):
     salary: float
 
 
-def parse_data_row(row) -> DataRow:
+def parse_data_row(row: str) -> DataRow:
+    """Read row and transform to NamedTuple with required attrs"""
     idx_to_read = [1, 2, 5]  # dep, team, salary
-    row = row.strip().split(";")
-    row = [row[idx] for idx in idx_to_read]
-    row[2] = float(row[2])  # convert salary to float
-    return DataRow(*row)
+    row_list = row.strip().split(";")
+    filtered_row: list = [row_list[idx] for idx in idx_to_read]
+    filtered_row[2] = float(filtered_row[2])
+    return DataRow(*filtered_row)
 
 
 def read_src_data(filename: str) -> list[DataRow]:
+    """Read source file and as list of DataRow"""
     data = []
     with Path(filename).open() as f:
         _ = f.readline()  # skip colnames
@@ -31,35 +33,45 @@ def read_src_data(filename: str) -> list[DataRow]:
 
 data = read_src_data("Corp_Summary.csv")
 
+DepName = str
+TeamName = str
 
-def sort_dep_teams(data=data):
+
+def sort_dict_by_key(dikt: dict) -> dict:
+    """Sort a dictionary by keys"""
+    return dict(sorted(dikt.items()))
+
+
+def sort_dep_teams(data: list[DataRow] = data) -> dict[DepName, list[TeamName]]:
+    """Read data and return dictionary with department and its team, sorted"""
     deps = defaultdict(set)
     for row in data:
         deps[row.dep].add(row.team)
     deps_sorted = {}
-    for dep, team in dict(sorted(deps.items())).items():
+    for dep, team in sort_dict_by_key(deps).items():
         deps_sorted[dep] = sorted(team)
     return deps_sorted
 
 
-def vis_dep_hierarchy(data=data):
+def get_deps_hierarchy(data: list[DataRow] = data) -> str:
+    """Get department hierarchy as formatted string"""
     deps = sort_dep_teams(data)
     string_out = []
     for dep, teams in deps.items():
         string_out.extend([dep, "\t" + "\n\t".join(teams)])
-    return "\n".join(string_out)  # return an actual string
-
-
-print(vis_dep_hierarchy(data))
+    return "\n".join(string_out)
 
 
 @dataclass
 class DepStatsRaw:
+    """Raw stats of department"""
+
     employee_cnt: int = 0
     salary_list: list[float] = field(default_factory=list)
 
 
-def get_data_for_stats(data=data) -> defaultdict[str, DepStatsRaw]:
+def get_data_for_stats(data=data) -> dict[DepName, DepStatsRaw]:
+    """Get raw stats for departments"""
     dep_stats_raw: defaultdict[str, DepStatsRaw] = defaultdict(DepStatsRaw)
     for row in data:
         dep_stats_raw[row.dep].employee_cnt += 1
@@ -70,12 +82,9 @@ def get_data_for_stats(data=data) -> defaultdict[str, DepStatsRaw]:
 raw_stats = get_data_for_stats(data)
 
 
-# print(raw_stats)
-def sort_dict(dikt):
-    return dict(sorted(dikt.items()))
-
-
 class DepStats(NamedTuple):
+    """Clean stats of department"""
+
     dep: str
     employee_cnt: int
     min_slry: float
@@ -83,9 +92,12 @@ class DepStats(NamedTuple):
     mean_slry: float
 
 
-def get_data_out(dep_stats_raw=raw_stats):
+def get_clean_stats(
+    dep_stats_raw: dict[DepName, DepStatsRaw] = raw_stats
+) -> list[DepStats]:
+    """Get clean stats of departments"""
     data_out = []
-    for dep, stats in sort_dict(dep_stats_raw).items():
+    for dep, stats in sort_dict_by_key(dep_stats_raw).items():
         slry = stats.salary_list
         min_slry = min(slry)
         max_slry = max(slry)
@@ -96,22 +108,27 @@ def get_data_out(dep_stats_raw=raw_stats):
     return data_out
 
 
-def get_deps_stats_pretty(stats: list[DepStats]):
+dep_stats = get_clean_stats(raw_stats)
+
+
+def get_deps_stats_pretty(stats: list[DepStats] = dep_stats) -> str:
+    """Make clean stats of departments as formatted string"""
     string_out = []
     for dep_stat in stats:
         string_out.append(
             f"{dep_stat.dep}\n"
-            f"\t{dep_stat.employee_cnt} {dep_stat.min_slry} {dep_stat.max_slry} {dep_stat.mean_slry}"
+            f"\tEmployee count: {dep_stat.employee_cnt:.0f}\n\t"
+            f"Mean salary: {dep_stat.mean_slry:.2f} "
+            f"(min={dep_stat.min_slry:.2f}, max={dep_stat.max_slry:.2f})"
         )
     return "\n".join(string_out)
 
 
-data_out = get_data_out(raw_stats)
-print(get_deps_stats_pretty(data_out))
-
-
-def write_csv_dep(data_out=data_out):
-    with Path("out.csv").open("w") as f:
+def write_csv_dep(
+    dep_stats: list[DepStats] = dep_stats, filename: str = "clean_stats_deps.csv"
+) -> str:
+    """Write clean Departments stats to CSV file"""
+    with Path(filename).open("w") as f:
         headers = [
             "dep_name",
             "employee_cnt",
@@ -120,35 +137,44 @@ def write_csv_dep(data_out=data_out):
             "mean_salary",
         ]
         f.write(";".join(headers) + "\n")
-        for row in data_out:
-            row = ";".join(map(str, row)) + "\n"
-            f.write(row)
+        for row in dep_stats:
+            str_row = ";".join(map(str, row)) + "\n"
+            f.write(str_row)
+        return f"Saved as {Path(filename)}"
 
 
 def menu() -> None:
+    """Choose available action and get results"""
+
     available_options = {
         1: "Print deps and teams",
         2: "Get stats on deps with cnt employees and salary",
         3: "Write stats on deps",
     }
-    print("\n".join(f"{num}. {command}" for num, command in available_options.items()))
+    str_options = "\n".join(
+        f"{num}. {command}" for num, command in available_options.items()
+    )
+    print(str_options)
+
     while True:
         try:
             choice = int(input("What's ur option, brother: "))
             if choice not in available_options:
-                print("Enter a valid number of command")
-            else:
-                idx_choose = choice - 1
-                break
+                raise ValueError
         except ValueError:
             print("Enter a number from 1 to 3")
-    #     might want to add else block if choice is successful
+        else:
+            break
 
-    ordered_options = [vis_dep_hierarchy, get_data_out, write_csv_dep]
-    option_to_func = dict(enumerate(ordered_options))
-    print(option_to_func[idx_choose]())
+    ordered_funcs_as_options = [
+        get_deps_hierarchy,
+        get_deps_stats_pretty,
+        write_csv_dep,
+    ]
+    option_to_func = dict(enumerate(ordered_funcs_as_options, start=1))
+    result: str = option_to_func[choice]()
+    print(result)
 
 
 if __name__ == "__main__":
-    pass
-    # menu()
+    menu()
