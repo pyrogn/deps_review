46e7d34cf3bd3bd47f08cab65e8e1cb4f160b4e5
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-09-29 17:08:13 +0300

    Fix csv output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import NamedTuple, Callable, Any
+from typing import NamedTuple
 
 
 class DataRow(NamedTuple):
